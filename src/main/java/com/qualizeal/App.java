94f95d40a5655898d7f5a7ea6b55802e6eb485d3
sushmitha <heysushmitha@gmail.com>
2024-10-24 11:06:47 +0530

    TwoNonDecimalNumbersSum

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,15 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp01.operation.TwoNonDecimalNumbersSum;
+
 public class App {
 	public static void main(String[] args) {
-
+		
+		TwoNonDecimalNumbersSum twoNonDecimalNumbersSum = new TwoNonDecimalNumbersSum();
+		
+		long result1 = twoNonDecimalNumbersSum.add(10, 2);
+		System.out.println(result1);
+		long result2 = twoNonDecimalNumbersSum.add(2, 10);
+		System.out.println(result2);
 	}
 }

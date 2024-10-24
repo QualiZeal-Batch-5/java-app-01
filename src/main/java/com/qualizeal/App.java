2189c29538a161bf014b70edaf05d0a249b7e301
Dandu Srija <dandusrija2503@gmail.com>
2024-10-24 11:20:07 +0530

    Performed addition operation on two non-decimal numbers

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,13 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp01.operation.TwoNonDecimalNumbersSum;
+
 public class App {
 	public static void main(String[] args) {
-
+		
+		TwoNonDecimalNumbersSum twoNonDecimalNumbersSum = new TwoNonDecimalNumbersSum();
+		long result = twoNonDecimalNumbersSum.add(2,3); 
+		System.out.println(result);
+		
 	}
 }

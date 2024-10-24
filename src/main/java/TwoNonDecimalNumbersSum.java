4ced869de6aae491ca9a304677550da9252e8dd4
Kattupalli Bhushana kumari <bhushana1805@gmail.com>
2024-10-24 11:01:14 +0530

    Performed addition operation on two non-decimal numbers.

---
+++ src/main/java/TwoNonDecimalNumbersSum.java
@@ -0,0 +1,15 @@
+package com.qualizeal.javaapp01.operation;
+
+public class TwoNonDecimalNumbersSum {
+	public long add(int num1, int num2) {
+		if(num1 == 0 || num2 == 0) {
+			return -2;
+		}
+		if(num1 < 0 || num2 < 0) {
+			return -1;
+		}
+		long result;
+		result = num1 + num2;
+		return result;
+	}
+}
--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,12 @@
-package com.qualizeal;
+package com.qualizeal.javaapp01;
+
+import com.qualizeal.javaapp01.operation.TwoNonDecimalNumbersSum;
 
 public class App {
 	public static void main(String[] args) {
-
+		TwoNonDecimalNumbersSum sum = new TwoNonDecimalNumbersSum();
+		System.out.println(sum.add(10, 20));
+		System.out.println(sum.add(0, 12));
+		System.out.println(sum.add(2, -90));
 	}
 }

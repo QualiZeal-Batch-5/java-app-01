cee308047c319ec5224b4e16c57f836d2def0850
PRIYANKA VEERANALA <priyankanaidu2003@gmail.com>
2024-10-24 11:54:26 +0530

    File Upload

---
+++ src/main/java/App.java
@@ -0,0 +1,7 @@
+import com.qualizeal.javaapp01.operation.TwoNonDecimalNumbersSum;
+public class App{
+	public static void main(String[] args){
+		TwoNonDecimalNumbersSum twoNonDecimalNumbersSum = new TwoNonDecimalNumbersSum();
+		twoNonDecimalNumbersSum.add(2,4);
+	}
+}

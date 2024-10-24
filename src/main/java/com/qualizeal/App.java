baf5986e7d1ef2d6ef009330e20ed16c0fbdcdbe
Swarna Modepalli <swarnamodepalli954@gmail.com>
2024-10-24 11:12:21 +0530

    Solution added.

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,11 @@
 package com.qualizeal;
-
+import com.qualizeal.javaapp01.operation.TwoNonDecimalNumbersSum;
 public class App {
 	public static void main(String[] args) {
-
+		TwoNonDecimalNumbersSum twoNonDecimalNumbersSum = new TwoNonDecimalNumbersSum();
+		long result1 = twoNonDecimalNumbersSum.add(10,20);
+		System.out.println(result1);
+		long result2 = twoNonDecimalNumbersSum.add(15,20);
+		System.out.println(result2);
 	}
 }

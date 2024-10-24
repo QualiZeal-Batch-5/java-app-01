cd4d506ead71f8b25504055b3a19d0f83bce9e5b
Keerthi Priya <keerthipriya1463@gmail.com>
2024-10-24 12:07:06 +0530

    Performed addition operation.

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,11 @@
-package com.qualizeal;
-
+package com.qualizeal.javaapp01;
+ 
+import com.qualizeal.javaapp01.operation.TwoNonDecimalNumbersSum;
+ 
 public class App {
 	public static void main(String[] args) {
-
+		TwoNonDecimalNumbersSum twoNonDecimalNumbersSum = new TwoNonDecimalNumbersSum();
+		long result = twoNonDecimalNumbersSum.add(10,2);
+        System.out.println(result);		
 	}
 }

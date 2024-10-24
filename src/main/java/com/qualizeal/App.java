a6152d61ec07deabf72be2831061d39831132cb3
Suhasini Cherukuri <suhasini.ch9600@gmail.com>
2024-10-24 11:37:29 +0530

    First Application

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,13 @@
 package com.qualizeal;
+import com.qualizeal.javaapp01.operation.TwoNonDecimalNumbersSum;
 
 public class App {
 	public static void main(String[] args) {
-
+		TwoNonDecimalNumbersSum twoNonDecimalNumbersSum = new TwoNonDecimalNumbersSum();
+		long result1 = twoNonDecimalNumbersSum.add(10,2);
+		System.out.println(result1);
+		long result2 = twoNonDecimalNumbersSum.add(12,13);
+		System.out.println(result2);
+		
 	}
 }

bfa38c04ca9ffaab6955e76fc1eac3a0774eea29
SnehareddyPasham <snehalathareddy3002@gmail.com>
2024-10-24 11:00:55 +0530

    added java assignment

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,8 @@
 package com.qualizeal;
-
+import com.qualizeal.javaapp01.operation.TwoNonDecimalNumbersSum;
 public class App {
 	public static void main(String[] args) {
-
+			TwoNonDecimalNumbersSum twodecimalnumberssum = new TwoNonDecimalNumbersSum();
+			twodecimalnumberssum.add(2,4);
 	}
 }

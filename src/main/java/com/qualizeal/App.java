04cc7facf6b333b927dc8014d2e61e2da73cd48d
Ramya Sree <ramyaganapavarapu87@gmail.com>
2024-10-24 11:21:12 +0530

    Pushed

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,10 @@
 package com.qualizeal;
+import com.qualizeal.javaapp01.operation.TwoNonDecimalNumbersSum;
 
 public class App {
 	public static void main(String[] args) {
+	 TwoNonDecimalNumbersSum a = new TwoNonDecimalNumbersSum();
+
 
 	}
 }

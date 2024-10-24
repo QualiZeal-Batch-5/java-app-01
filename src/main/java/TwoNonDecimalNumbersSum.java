f973d5c624e5d4450a1943c82278c7d5d74b60a0
chandini <chandinimalla3@gmail.com>
2024-10-24 11:00:53 +0530

    solution added

---
+++ src/main/java/TwoNonDecimalNumbersSum.java
@@ -0,0 +1,15 @@
+package com.qualizeal.javaapp01.operation;
+public class TwoNonDecimalNumbersSum{
+	public long add(int num1,int num2){
+		int result=0;
+		if(num1<0||num2<0){
+			return -1;
+		}
+		else if(num1==0||num2==0){
+			return -2;
+		}
+		else{
+			return result=num1+num2;
+		}
+	}
+}
